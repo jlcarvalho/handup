7c6f41a5171dbb3749ff92fb032180c0f85548c6
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-01-04 10:26:53 -0200

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBkZlNEndLOCKUS0EeSB6Ls-F3Jr2tG26o",
+  authDomain: "handup-71ab8.firebaseapp.com",
+  databaseURL: "https://handup-71ab8.firebaseio.com",
+  storageBucket: "handup-71ab8.appspot.com",
+  messagingSenderId: "862725985035"
+}
 
 @NgModule({
   declarations: [
@@ -9,7 +18,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
