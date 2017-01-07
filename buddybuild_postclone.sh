f5ec76cd0a875c5189792501cf6acffa8d02b208
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-01-07 12:52:25 -0200

    Buddy buildpostclone

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+cd /Users/buddybuild/workspace
+
 echo '=== Karma Test Environemnt ==='
 npm install -g karma
 npm install -g karma-cli
+
+echo '=== Install cordova and ionic@beta'
+npm install -g cordova ionic
+
+echo '=== Start to run : npm install'
+npm install
+
+echo '=== Start to run : ionic info'
+ionic info
+
+echo '=== Start to run : ionic add platforms'
+ionic platform add ios
+ionic platform add android
+
+echo '=== Start to run : ionic state restore'
+ionic prepare
+ionic platform list
+
+echo '=== Start to run : env'
+env
+
+if hash android 2>/dev/null; then
+    echo '=== Detected android command, run android list sdk --all'
+    android list sdk --all
+fi
